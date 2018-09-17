700d9cd1f99ef29e5660242878c96c9ab587e954
fsschmitt <felipeschmitt@Felipes-MacBook-Pro.local>
2018-09-17 17:53:07 +0100

    Updated tests to include testing of the fetch machines grouped by tags method's logic
    Added test_mocks.py file to include the mock methods used to mock the MAAS API calls.
    
    Updated tests_fixtures.py to include the API mock responses.
    
    Added new test on test_inventories.py to test the method's call.

--- test_inventories.py
+++ test_inventories.py
@@ -1,12 +1,22 @@
 import unittest
+from unittest.mock import MagicMock
+from inventory import Fetcher
 from inventory import InventoryBuilder
+import test_mocks as mocks
 import test_fixtures as fixtures
-
 
 class TestInventoryBuilderMethods(unittest.TestCase):
 
     def setUp(self):
+        self.inventory_fetcher = Fetcher(None, None)
         self.inventory_builder = InventoryBuilder()
+
+    def test_fetch_machines_grouped_by_tags(self):
+        self.inventory_fetcher._fetch_tags_summary = MagicMock(side_effect=mocks.fetch_tags_summary)
+        self.inventory_fetcher._fetch_machines_by_tag = MagicMock(side_effect=mocks.fetch_machines_by_tag)
+        machines_by_tag = self.inventory_fetcher.fetch_machines_grouped_by_tags()
+
+        self.assertEqual(machines_by_tag,fixtures.test_fetch_machines_grouped_by_tags_output)
 
     def test_build_from_tagged_machines(self):
         inventory = self.inventory_builder.build_from_tagged_machines(
